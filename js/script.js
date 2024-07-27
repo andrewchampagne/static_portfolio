001c239188746aaa2708a99b0588ebf7ad8a6efc
andrewchampagne <114505093+andrewchampagne@users.noreply.github.com>
2024-07-26 20:01:51 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 function sendMail(){
-    console.log("first step")
     var params = {
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
         body: document.getElementById("body").value
     }
-    console.log("made it here")
     emailjs.send("service_kyv692r", "template_1a1l5mj",params).then(function (res){
         alert("Successfully sent.")
     })
-    console.log("wow we did it!")
+    console.log("success")
 }
